b9bc4ffbe1dd62d00edcb9df0e9cf6a5ed2be6ad
Joshua Morony <joshua.morony@gmail.com>
2023-08-11 20:26:03 +0930

    test: e2e test for creating checklist

--- tests/home/home.po.ts
+++ tests/home/home.po.ts
@@ -1,21 +1,27 @@
-import { Locator, Page } from '@playwright/test';
+import { Locator, Page } from "@playwright/test";
 
 export class HomePage {
   page: Page;
   createChecklistButton: Locator;
+  titleInput: Locator;
+  saveChecklistButton: Locator;
   noChecklistsMessage: Locator;
 
   constructor(page: Page) {
     this.page = page;
-    this.createChecklistButton = page.getByTestId('create-checklist-button');
-    this.noChecklistsMessage = page.getByText('create your first');
+    this.createChecklistButton = page.getByTestId("create-checklist-button");
+    this.saveChecklistButton = page.getByTestId("save-checklist-button");
+    this.titleInput = page.getByLabel("title");
+    this.noChecklistsMessage = page.getByText("create your first");
   }
 
   async goto() {
-    await this.page.goto('/home');
+    await this.page.goto("/home");
   }
 
-  async createChecklist() {
+  async createChecklist(title: string) {
     await this.createChecklistButton.click();
+    await this.titleInput.fill(title);
+    await this.saveChecklistButton.click();
   }
 }
--- tests/home/home.spec.ts
+++ tests/home/home.spec.ts
@@ -1,13 +1,22 @@
-import { test, expect, type Page } from '@playwright/test';
-import { HomePage } from './home.po';
+import { test, expect, type Page } from "@playwright/test";
+import { HomePage } from "./home.po";
 
 let homePage: HomePage;
+let currentPage: Page;
 
 test.beforeEach(async ({ page }) => {
   homePage = new HomePage(page);
+  currentPage = page;
   await homePage.goto();
 });
 
-test('no checklists message displayed', async () => {
+test("no checklists message displayed", async () => {
   await expect(homePage.noChecklistsMessage).toBeVisible();
+});
+
+test("can add checklists", async () => {
+  const testTitle = "my test checklist";
+  await homePage.createChecklist(testTitle);
+
+  expect(currentPage.getByText(testTitle)).toBeVisible();
 });
